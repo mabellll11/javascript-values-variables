ed82bd08302bbd60fc78f117b854b2f78885e643
Mabell;) <wanwisadumjuy@gmail.com>
2025-12-16 23:49:05 +0700

    feat:r-page

--- ex-1and2.js
+++ ex-1and2.js
@@ -3,3 +3,16 @@
 
 //Exercise #2: Variable Reassignment
 //Start coding here
+const userName = "Karter";
+const lastName = "Anderson";
+const shippingAddress = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai";
+const restaurantName = "Chef's Table by Chef James";
+let netFoodPrice = 15000;
+let openRestaurantsCount = 1500;
+
+netFoodPrice = 5000;
+openRestaurantsCount = 2500;
+
+console.log(shippingAddress);
+console.log(netFoodPrice);
+console.log(openRestaurantsCount);
